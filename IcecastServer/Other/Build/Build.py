d9b92133732a28426ed3f496948607b52537b723
Marius Göcke <marius.goecke@gmail.com>
2026-09-15 23:44:56 +0200

    pined icecast version

--- IcecastServer/Other/Build/Build.py
+++ IcecastServer/Other/Build/Build.py
@@ -1,4 +1,5 @@
 from ScriptCollection.GeneralUtilities import Platform
+import os
 from ScriptCollection.TFCPS.Docker.TFCPS_CodeUnitSpecific_Docker import TFCPS_CodeUnitSpecific_Docker_Functions,TFCPS_CodeUnitSpecific_Docker_CLI
 
 
@@ -10,6 +11,7 @@ def build():
     tf:TFCPS_CodeUnitSpecific_Docker_Functions=TFCPS_CodeUnitSpecific_Docker_CLI.parse(__file__)
     tf.build(platforms,{
         "image_alpine":tf.tfcps_Tools_General.oci_image_manager.get_registry_address_for_image_with_default_tag(tf.get_repository_folder(),"Alpine"),
+        "icecast_version":tf.tfcps_Tools_General.get_dependency_version_in_resources_folder(os.path.join(tf.get_codeunit_folder(),"Other","Resources"), "Icecast"),
     })
 
 
--- IcecastServer/Other/UpdateDependencies.py
+++ IcecastServer/Other/UpdateDependencies.py
@@ -1,6 +1,18 @@
+from pathlib import Path
+from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
+from ScriptCollection.TFCPS.TFCPS_Tools_General import TFCPS_Tools_General
+from ScriptCollection.TFCPS.Docker.TFCPS_CodeUnitSpecific_Docker import TFCPS_CodeUnitSpecific_Docker_Functions,TFCPS_CodeUnitSpecific_Docker_CLI
+from ScriptCollection.OCIImages.OCIImageManager import OCIImageManager
+
 
 def update_dependencies():
-    pass# TODO get and save latest icecast version number
+    script_file = str(Path(__file__).absolute())
+    sc = ScriptCollectionCore()
+    tf:TFCPS_CodeUnitSpecific_Docker_Functions=TFCPS_CodeUnitSpecific_Docker_CLI.parse(__file__)
+    oci_image_manager:OCIImageManager=OCIImageManager(sc)
+    image=oci_image_manager.get_registry_address_for_image_with_default_tag(tf.get_repository_folder(),"Alpine")
+    tg=TFCPS_Tools_General(sc)
+    tg.update_dependency_in_resources_folder(script_file, "Icecast", sc.get_latest_apk_package_version_in_alpine( image, "icecast"))
 
 
 if __name__ == "__main__":
